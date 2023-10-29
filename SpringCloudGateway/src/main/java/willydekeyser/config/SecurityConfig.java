09ba9ae09488387443f130eb9cb37cafcd9badf9
willydekeyser <ma074172@skynet.be>
2023-10-29 17:18:43 +0100

    Update Spring Cloud Gateway

--- SpringCloudGateway/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringCloudGateway/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -1,23 +1,82 @@
 package willydekeyser.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestHandler;
+import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
+import org.springframework.web.server.WebFilter;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
+	private final ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
+	
+	public SecurityConfig(ReactiveClientRegistrationRepository reactiveClientRegistrationRepository) {
+		this.reactiveClientRegistrationRepository = reactiveClientRegistrationRepository;
+	}
+	
 	@Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, 
+    		ServerOAuth2AuthorizationRequestResolver resolver) {
+		CookieServerCsrfTokenRepository tokenRepository = CookieServerCsrfTokenRepository.withHttpOnlyFalse();
+	    tokenRepository.setCookiePath("/");
+	    tokenRepository.setHeaderName("X-XSRF-TOKEN");
+	    XorServerCsrfTokenRequestAttributeHandler delegate = new XorServerCsrfTokenRequestAttributeHandler();
+	    ServerCsrfTokenRequestHandler requestHandler = delegate::handle;
 		return http
                 .authorizeExchange(exchange -> exchange
                 		.pathMatchers("/**").permitAll()
                 		.anyExchange().authenticated())
-                .csrf(withDefaults())
-                .oauth2Login(withDefaults())
+                .logout(logout -> logout
+                		.logoutUrl("/logout")
+                		.logoutSuccessHandler(serverLogoutSuccessHandler(reactiveClientRegistrationRepository)))
+                .cors(withDefaults())
+                .csrf(csrf -> csrf
+                		.csrfTokenRepository(tokenRepository)
+            			.csrfTokenRequestHandler(requestHandler)
+                		)
+                .oauth2Login(auth -> auth
+                		.authorizationRequestResolver(resolver))
                 .oauth2Client(withDefaults())
                 .build();
     }
+	
+	@Bean
+	WebFilter csrfCookieWebFilter() {
+		return (exchange, chain) -> {
+			Mono<CsrfToken> csrfToken = exchange.getAttributeOrDefault(CsrfToken.class.getName(), Mono.empty());
+			return csrfToken.doOnSuccess(token -> {
+			}).then(chain.filter(exchange));
+		};
+	}
+	
+	@Bean
+	ServerLogoutSuccessHandler serverLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
+	       OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+	       oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/logged-out");
+	       return oidcLogoutSuccessHandler;
+	}
+	
+	@Bean
+	ServerOAuth2AuthorizationRequestResolver pkceResolver(ReactiveClientRegistrationRepository repo) {
+	    var resolver = new DefaultServerOAuth2AuthorizationRequestResolver(repo);
+	    resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+	    return resolver;
+	}
 }
--- SpringResourceServer/src/main/java/willydekeyser/controller/HomeController.java
+++ SpringResourceServer/src/main/java/willydekeyser/controller/HomeController.java
@@ -2,8 +2,8 @@ package willydekeyser.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +16,7 @@ public class HomeController {
 	}
 	
 	@PostMapping("/")
-	public String postHome(@RequestParam("id") String id) {
+	public String postHome(@RequestBody String id) {
 		return "Spring Resource Server 01 " + id;
 	}
 }
